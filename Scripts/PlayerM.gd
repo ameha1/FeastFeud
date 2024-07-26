b4162d05d3846d4a7c39a755d8089882a213fb02
Ameha Seyoum <129897712+ameha1@users.noreply.github.com>
2024-07-26 23:14:59 +0100

    complete player movement

--- Scripts/PlayerM.gd
+++ Scripts/PlayerM.gd
@@ -11,13 +11,13 @@ var facing_direction = 0
 #movement constants
 const MAX_SPEED = 20
 const ACCEL = 5
-const DECCEL = 10
+const DECCEL = 15
 const JUMP_SPEED = 15
 const GRAVITY = -45
 
 #animation constants
 const BLEND_MIN = 0.125
-const IDLE_BLEND_AMOUNT = 0.05
+const IDLE_BLEND_AMOUNT = 0.5
 const RUN_BLEND_AMOUNT = 0.1
 
 #animation variables
@@ -32,41 +32,71 @@ func move(delta):
 	var movement_dir = get_2D_movement()
 	var camera_xform = $Camera.get_global_transform()
 	
-	dir = camera_xform.basis.Z.normalized() * movement_dir.Y
-	dir = camera_xform.basis.X.normalized() * movement_dir.X
+	dir = Vector3(0,0,0)
+	
+	dir += camera_xform.basis.z.normalized() * movement_dir.y
+	dir += camera_xform.basis.x.normalized() * movement_dir.x
 	
 	dir = move_vertically(dir, delta)
 	vel = h_accel(dir, delta)
 	
 	
-	move_and_slide(dir*speed, UP)
+	move_and_slide(vel, UP)
 
 
 func get_2D_movement():
 	var movemnt_vector = Vector2()
 	
 	if Input.is_action_pressed("forward") and not Input.is_action_pressed("back"):
-		movemnt_vector.y = 1
+		movemnt_vector.y = -1
 		facing_direction = 0
 	if Input.is_action_pressed("back") and not Input.is_action_pressed("forward"):
-		movemnt_vector.y = -1
+		movemnt_vector.y = 1
 		facing_direction = PI
 	if Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
-		movemnt_vector.x = -1
+		movemnt_vector.x = 1
 		facing_direction = PI * 1.5
 	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
-		movemnt_vector.x = 1
+		movemnt_vector.x = -1
 		facing_direction = PI * 0.5
 		
 	return movemnt_vector.normalized()
 
 
 func move_vertically(dir, delta):
-	pass
+	vel.y += GRAVITY*delta
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		vel.y = JUMP_SPEED
+	elif is_on_floor():
+		vel.y = 0
+		
+	dir.y = 0
+	dir = dir.normalized()
+	
+	return dir
 
 
 func h_accel(dir, delta):
-	pass
+	var vel_2D = vel
+	vel_2D.y = 0
+	
+	var target = dir
+	target *= MAX_SPEED
+	
+	var accel
+	
+	if dir.dot(vel_2D) > 0:
+		accel = ACCEL
+	else:
+		accel = DECCEL
+		
+	vel_2D = vel_2D.linear_interpolate(target, accel*delta)
+	
+	vel.x = vel_2D.x
+	vel.z = vel_2D.z
+	
+	return vel
 
 
 func facing_forward():
